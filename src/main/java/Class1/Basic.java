d5a0c4f99c512cadfc18f1a077391470cc627e81
Faruk <Faruk@Azhaan.lan>
2024-06-29 10:54:44 -0400

    commit 3

--- src/main/java/Class1/Basic.java
+++ src/main/java/Class1/Basic.java
@@ -5,7 +5,8 @@ public class Basic {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git Hub Test");
-		System.out.println();
+		System.out.println(1);
+		System.out.println(2);
 		
 
 	}
